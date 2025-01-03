76770afc649fabba85167e7844f42f3fe70dc0bb
Delwin Perera <perera@mm.tu-darmstadt.de>
2025-01-03 19:33:26 +0100

    Add zsh configuration files
    Signed-off-by: Delwin Perera <perera@mm.tu-darmstadt.de>

---
+++ dot_config/oh-my-zsh/custom/keybindings.zsh
@@ -0,0 +1,23 @@
+# up
+function up_widget() {
+    BUFFER="cd .."
+    zle accept-line
+}
+zle -N up_widget
+bindkey "^k" up_widget
+
+# Edit and rerun
+function edit_and_run() {
+    BUFFER="fc"
+    zle accept-line
+}
+zle -N edit_and_run
+bindkey "^v" edit_and_run
+
+# Sudo
+function add_sudo() {
+    BUFFER="sudo "$BUFFER
+    zle end-of-line
+}
+zle -N add_sudo
+bindkey "^s" add_sudo
